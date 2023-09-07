09c37a65e721529424d57c744a267629dcd4a5af
ldurniat <ldurniat@gmail.com>
2023-09-07 18:47:26 +0200

    Add main section

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
     <header>
         <h1>My Music</h1>
     </header>
+    <main>
+        <section class="player">
+            <h2 class="song-title">Test</h2>
+        </section>
+    </main>
 </template>
 
 <style scoped>
